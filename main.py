cae700d29b9a33ebd6532d8f8a179df8b731f15c
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-09-13 16:16:05 -0300

    Add write_multiple_sheets to main.py

--- main.py
+++ main.py
@@ -39,3 +39,32 @@ def write_and_read_xls():
         print("%s is %d years old and works as a %s developer" % (record['Name'], record['Age'], record['Job']))
 
 write_and_read_xls()
+
+def write_multiple_sheets():
+    content = {
+        'Sheet 1':
+            [
+                [1.0, 2.0, 3.0],
+                [4.0, 5.0, 6.0],
+                [7.0, 8.0, 9.0]
+            ],
+        'Sheet 2':
+            [
+                ['X', 'Y', 'Z'],
+                [1.0, 2.0, 3.0],
+                [4.0, 5.0, 6.0]
+            ],
+        'Sheet 3':
+            [
+                ['O', 'P', 'Q'],
+                [3.0, 2.0, 1.0],
+                [4.0, 3.0, 2.0]
+            ]
+    }
+    book = p.get_book(bookdict=content)
+    book.save_as("output.xls")
+    records = p.iget_records(file_name="output.xls")
+    for record in records:
+        print(record)
+
+write_multiple_sheets()
