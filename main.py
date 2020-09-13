41a8de3eb1f9e5968a01ee2c2ee7c33368e0ccc8
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-09-13 01:36:49 -0300

    Add read records functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pyexcel
+import pyexcel as p
 
+# Records that will be added to the .xls file
 a_list_of_dictionaries = [
    {
        "Name": 'Linus',
@@ -23,4 +24,10 @@ a_list_of_dictionaries = [
    }
 ]
 
-pyexcel.save_as(records=a_list_of_dictionaries, dest_file_name="test.xls")
+# Edits .xls
+p.save_as(records=a_list_of_dictionaries, dest_file_name="test.xls")
+
+# Reads .xls's records
+records = p.iget_records(file_name="test.xls")
+for record in records:
+    print("%s is aged at %d and works as a %s developer" % (record['Name'], record['Age'], record['Job']))
